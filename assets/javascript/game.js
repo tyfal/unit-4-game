6ef95042093f6280c999b6de2b832f8269e69500
tyfal <tfalcoff@gmail.com>
2019-07-06 11:02:14 -0600

    setting up game pseudo code and html grid

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+class character {
+
+    // constructor - pass image and player/enemy status
+    
+
+    // attack - increase incrementally with each pass
+
+
+    // takeHit - decrease with each attack from opponent 
+
+
+}
+
+
+class battle {
+
+    // constructor - pass players
+        
+    
+    // battle - for each player set up player/enemy stand off
+    
+    
+    // victor - determining victor
+
+
+}
